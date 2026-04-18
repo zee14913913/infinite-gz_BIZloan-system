1a432ae36532c41b982f511fefa44106dfab9601
INFINITE GZ Dev <dev@infinitegz.my>
2026-04-18 06:54:32 +0000

    Schema: add product_rules_json, product_signals_json, screening_view_json to Product

---
+++ prisma/migrations/20260418065429_add_intelligence_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN "product_rules_json" TEXT;
+ALTER TABLE "Product" ADD COLUMN "product_signals_json" TEXT;
+ALTER TABLE "Product" ADD COLUMN "screening_view_json" TEXT;
